4b402c9b7e444b6721faafae42863db61b294683
MrCroxx <mrcroxx@outlook.com>
2022-05-14 18:12:28 +0800

    test: restore integration tests

--- tests/integrations/test_multi_raft_group_concurrent_put_get.rs
+++ tests/integrations/test_multi_raft_group_concurrent_put_get.rs
@@ -1,259 +1,53 @@
-use std::collections::HashMap;
-use std::fs::read_to_string;
 use std::path::Path;
-use std::sync::Arc;
-use std::time::Duration;
 
-use futures::future;
-use itertools::Itertools;
-use rand::{thread_rng, Rng};
-use runkv_exhauster::config::ExhausterConfig;
-use runkv_exhauster::{bootstrap_exhauster, build_exhauster_with_object_store};
-use runkv_proto::common::Endpoint;
-use runkv_proto::kv::kv_service_client::KvServiceClient;
-use runkv_proto::kv::{DeleteRequest, GetRequest, PutRequest};
-use runkv_proto::meta::KeyRange;
-use runkv_proto::wheel::wheel_service_client::WheelServiceClient;
-use runkv_proto::wheel::{AddEndpointsRequest, AddKeyRangeRequest};
-use runkv_rudder::config::RudderConfig;
-use runkv_rudder::{bootstrap_rudder, build_rudder_with_object_store};
-use runkv_storage::MemObjectStore;
-use runkv_wheel::config::WheelConfig;
-use runkv_wheel::{bootstrap_wheel, build_wheel_with_object_store};
+use runkv_tests::{run, Args, Options};
 use test_log::test;
-use tonic::transport::Channel;
-use tonic::Request;
-use tracing::trace;
 
-use crate::concat_toml;
-
-const RUDDER_CONFIG_PATH: &str = "etc/rudder.toml";
-const WHEEL_CONFIG_PATH: &str = "etc/wheel.toml";
-const EXHAUSTER_CONFIG_PATH: &str = "etc/exhauster.toml";
-const LSM_TREE_CONFIG_PATH: &str = "etc/lsm_tree.toml";
-
-const CONCURRENCY: u64 = 1000;
-const LOOP: u64 = 3;
-
-async fn add_key_range(
-    wheel_client: &mut WheelServiceClient<Channel>,
-    start: &[u8],
-    end: &[u8],
-    group: u64,
-    raft_nodes: &[u64],
-    node: u64,
-) {
-    wheel_client
-        .add_key_range(Request::new(AddKeyRangeRequest {
-            key_range: Some(KeyRange {
-                start_key: start.to_vec(),
-                end_key: end.to_vec(),
-            }),
-            group,
-            raft_nodes: raft_nodes.to_vec(),
-            nodes: HashMap::from_iter(raft_nodes.iter().map(|&raft_node| (raft_node, node))),
-        }))
-        .await
-        .unwrap();
-}
-
-async fn add_key_ranges(wheel_client: &mut WheelServiceClient<Channel>, node: u64) {
-    add_key_range(wheel_client, b"k0", b"k0z", 10, &[11, 12, 13], node).await;
-    add_key_range(wheel_client, b"k1", b"k1z", 20, &[21, 22, 23], node).await;
-    add_key_range(wheel_client, b"k2", b"k2z", 30, &[31, 32, 33], node).await;
-    add_key_range(wheel_client, b"k3", b"k3z", 40, &[41, 42, 43], node).await;
-    add_key_range(wheel_client, b"k4", b"k4z", 50, &[51, 52, 53], node).await;
-    add_key_range(wheel_client, b"k5", b"k5z", 60, &[61, 62, 63], node).await;
-    add_key_range(wheel_client, b"k6", b"k6z", 70, &[71, 72, 73], node).await;
-    add_key_range(wheel_client, b"k7", b"k7z", 80, &[81, 82, 83], node).await;
-    add_key_range(wheel_client, b"k8", b"k8z", 90, &[91, 92, 93], node).await;
-    add_key_range(wheel_client, b"k9", b"k9z", 100, &[101, 102, 103], node).await;
-}
+use crate::*;
 
 #[test(tokio::test)]
-async fn test_multi_raft_group_concurrent_put_get() {
-    let mut port = crate::port("test_multi_raft_group_concurrent_put_get");
+async fn test_concurrent_put_get() {
+    let port = crate::port("test_multi_raft_group_concurrent_put_get");
+
+    let options = Options {
+        log: false,
+        rudder_config_path: RUDDER_CONFIG_PATH.to_string(),
+        wheel_config_path: WHEEL_CONFIG_PATH.to_string(),
+        exhauster_config_path: EXHAUSTER_CONFIG_PATH.to_string(),
+        lsm_tree_config_path: LSM_TREE_CONFIG_PATH.to_string(),
+        rudder_node_id: 10000,
+        wheel_node_id_base: 0,
+        exhauster_node_id_base: 100,
+        rudder_port: port,
+        exhauster_port_base: port,
+        wheel_port_base: port + 1,
+        wheel_prometheus_port_base: 0,
+    };
 
     let tempdir = tempfile::tempdir().unwrap();
-    let raft_log_dir_path = Path::new(tempdir.path())
+    let raft_log_store_data_dir = Path::new(tempdir.path())
         .join("raft")
         .to_str()
         .unwrap()
         .to_string();
+    let log_dir = Path::new(tempdir.path())
+        .join("log")
+        .to_str()
+        .unwrap()
+        .to_string();
 
-    let rudder_config: RudderConfig = {
-        let mut config: RudderConfig =
-            toml::from_str(&concat_toml(RUDDER_CONFIG_PATH, LSM_TREE_CONFIG_PATH)).unwrap();
-        port += 1;
-        config.port = port;
-        config
-    };
-    let wheel_config: WheelConfig = {
-        let mut config: WheelConfig =
-            toml::from_str(&concat_toml(WHEEL_CONFIG_PATH, LSM_TREE_CONFIG_PATH)).unwrap();
-        config.raft_log_store.log_dir_path = raft_log_dir_path;
-        port += 1;
-        config.port = port;
-        config.rudder.port = rudder_config.port;
-        config
-    };
-    let exhauster_config: ExhausterConfig = {
-        let mut config: ExhausterConfig =
-            toml::from_str(&read_to_string(EXHAUSTER_CONFIG_PATH).unwrap()).unwrap();
-        port += 1;
-        config.port = port;
-        config.rudder.port = rudder_config.port;
-        config
+    let args = Args {
+        wheels: 1,
+        exhausters: 1,
+        groups: 10,
+        key_size: 64,
+        value_size: 64,
+        concurrency: 100,
+        r#loop: 3,
+        raft_log_store_data_dir,
+        persist: "sync".to_string(),
+        log_dir,
     };
 
-    let object_store = Arc::new(MemObjectStore::default());
-
-    let (rudder, rudder_workers) =
-        build_rudder_with_object_store(&rudder_config, object_store.clone())
-            .await
-            .unwrap();
-
-    let (wheel, wheel_workers) = build_wheel_with_object_store(&wheel_config, object_store.clone())
-        .await
-        .unwrap();
-
-    let (exhuaster, exhauster_workers) =
-        build_exhauster_with_object_store(&exhauster_config, object_store)
-            .await
-            .unwrap();
-
-    tokio::spawn(async move { bootstrap_rudder(&rudder_config, rudder, rudder_workers).await });
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    tokio::spawn(async move {
-        bootstrap_exhauster(&exhauster_config, exhuaster, exhauster_workers).await
-    });
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    let wheel_config_clone = wheel_config.clone();
-    tokio::spawn(async move { bootstrap_wheel(&wheel_config_clone, wheel, wheel_workers).await });
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    // TODO: Refine me.
-    let mut wheel_client = WheelServiceClient::connect(format!(
-        "http://{}:{}",
-        wheel_config.host, wheel_config.port
-    ))
-    .await
-    .unwrap();
-    wheel_client
-        .add_endpoints(AddEndpointsRequest {
-            endpoints: HashMap::from_iter([(
-                wheel_config.id,
-                Endpoint {
-                    host: wheel_config.host.to_owned(),
-                    port: wheel_config.port as u32,
-                },
-            )]),
-        })
-        .await
-        .unwrap();
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    add_key_ranges(&mut wheel_client, wheel_config.id).await;
-
-    tokio::time::sleep(Duration::from_secs(10)).await;
-
-    let channel = tonic::transport::Endpoint::from_shared(format!(
-        "http://{}:{}",
-        wheel_config.host, wheel_config.port
-    ))
-    .unwrap()
-    .connect()
-    .await
-    .unwrap();
-
-    let futures = (0..CONCURRENCY)
-        .map(|i| {
-            let channel_clone = channel.clone();
-            async move {
-                for _ in 0..LOOP {
-                    let mut rng = thread_rng();
-                    let channel_clone_clone = channel_clone.clone();
-                    let mut client = KvServiceClient::new(channel_clone_clone);
-                    tokio::time::sleep(Duration::from_millis(rng.gen_range(0..100))).await;
-                    trace!("put {:?}", key(i));
-                    client
-                        .put(Request::new(PutRequest {
-                            key: key(i),
-                            value: value(i),
-                        }))
-                        .await
-                        .unwrap();
-                    tokio::time::sleep(Duration::from_millis(rng.gen_range(0..100))).await;
-                    trace!("get {:?}", key(i));
-                    assert_eq!(
-                        client
-                            .get(Request::new(GetRequest {
-                                key: key(i),
-                                sequence: 0,
-                            }))
-                            .await
-                            .unwrap()
-                            .into_inner()
-                            .value,
-                        value(i)
-                    );
-                    tokio::time::sleep(Duration::from_millis(rng.gen_range(0..100))).await;
-                    trace!("delete {:?}", key(i));
-                    client
-                        .delete(Request::new(DeleteRequest { key: key(i) }))
-                        .await
-                        .unwrap();
-                    tokio::time::sleep(Duration::from_millis(rng.gen_range(0..100))).await;
-                    trace!("get {:?}", key(i));
-                    assert_eq!(
-                        client
-                            .get(Request::new(GetRequest {
-                                key: key(i),
-                                sequence: 0,
-                            }))
-                            .await
-                            .unwrap()
-                            .into_inner()
-                            .value,
-                        vec![]
-                    );
-                    tokio::time::sleep(Duration::from_millis(rng.gen_range(0..100))).await;
-                    trace!("put {:?}", key(i));
-                    client
-                        .put(Request::new(PutRequest {
-                            key: key(i),
-                            value: value(i),
-                        }))
-                        .await
-                        .unwrap();
-                    tokio::time::sleep(Duration::from_millis(rng.gen_range(0..100))).await;
-                    trace!("get {:?}", key(i));
-                    assert_eq!(
-                        client
-                            .get(Request::new(GetRequest {
-                                key: key(i),
-                                sequence: 0,
-                            }))
-                            .await
-                            .unwrap()
-                            .into_inner()
-                            .value,
-                        value(i)
-                    );
-                }
-            }
-        })
-        .collect_vec();
-
-    future::join_all(futures).await;
-}
-
-fn key(i: u64) -> Vec<u8> {
-    format!("k{:03}{:61}", i, 0).as_bytes().to_vec()
-}
-
-fn value(i: u64) -> Vec<u8> {
-    format!("v{:03}{:61}", i, 0).as_bytes().to_vec()
+    run(args, options).await;
 }
---
+++ tests/src/lib.rs
@@ -0,0 +1,398 @@
+use std::collections::HashMap;
+use std::fs::read_to_string;
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+
+use bytes::BufMut;
+use clap::Parser;
+use futures::future;
+use itertools::Itertools;
+use rand::{thread_rng, Rng};
+use runkv_client::client::{RunkvClient, RunkvClientOptions};
+use runkv_common::log::init_runkv_logger;
+use runkv_common::time::timestamp;
+use runkv_exhauster::config::ExhausterConfig;
+use runkv_exhauster::{bootstrap_exhauster, build_exhauster_with_object_store};
+use runkv_proto::common::Endpoint;
+use runkv_proto::meta::{KeyRange, KeyRangeInfo};
+use runkv_proto::rudder::control_service_client::ControlServiceClient;
+use runkv_proto::rudder::{AddKeyRangesRequest, AddWheelsRequest};
+use runkv_rudder::config::RudderConfig;
+use runkv_rudder::{bootstrap_rudder, build_rudder_with_object_store};
+use runkv_storage::MemObjectStore;
+use runkv_wheel::config::WheelConfig;
+use runkv_wheel::{bootstrap_wheel, build_wheel_with_object_store};
+use tonic::transport::Channel;
+use tonic::Request;
+
+const LOCALHOST: &str = "127.0.0.1";
+
+#[derive(Clone, Debug)]
+pub struct Options {
+    pub log: bool,
+    pub rudder_config_path: String,
+    pub wheel_config_path: String,
+    pub exhauster_config_path: String,
+    pub lsm_tree_config_path: String,
+    pub rudder_node_id: u64,
+    pub wheel_node_id_base: u64,
+    pub exhauster_node_id_base: u64,
+    pub rudder_port: u16,
+    pub wheel_port_base: u16,
+    pub wheel_prometheus_port_base: u16,
+    pub exhauster_port_base: u16,
+}
+
+#[derive(Parser, Clone, Debug)]
+pub struct Args {
+    /// Count of wheel nodes, [1, 10].
+    #[clap(long, default_value = "1")]
+    pub wheels: u64,
+
+    /// Count of exhauster nodes, [1, 10].
+    #[clap(long, default_value = "1")]
+    pub exhausters: u64,
+
+    /// Count of raft groups, [1, 100].
+    #[clap(long, default_value = "10")]
+    pub groups: u8,
+
+    /// Key size (B), [10, 4096].
+    #[clap(long, default_value = "64")]
+    pub key_size: usize,
+
+    /// Valuw size (B), [10, 4096].
+    #[clap(long, default_value = "64")]
+    pub value_size: usize,
+
+    /// Concurrency of each raft group.
+    #[clap(long, default_value = "100")]
+    pub concurrency: u64,
+
+    /// Loop time of each coroutine.
+    #[clap(long, default_value = "100")]
+    pub r#loop: u64,
+
+    #[clap(long, default_value = ".run/tmp/bench-kv/raft-log-store-data")]
+    pub raft_log_store_data_dir: String,
+
+    #[clap(long, default_value = "sync")]
+    pub persist: String,
+
+    #[clap(long, default_value = ".run/tmp/bench-kv/log")]
+    pub log_dir: String,
+}
+
+fn concat_toml(path1: &str, path2: &str) -> String {
+    let mut s = String::default();
+    s.push_str(&read_to_string(path1).unwrap());
+    s.push('\n');
+    s.push_str(&read_to_string(path2).unwrap());
+    s
+}
+
+fn key(group: u8, i: u64, size: usize) -> Vec<u8> {
+    let mut buf = Vec::with_capacity(size);
+    buf.put_u8(b'k');
+    buf.put_u8(group);
+    buf.put_u64(i);
+    buf.put_slice(&vec![b' '; size - 10]);
+    buf
+}
+
+fn value(group: u8, i: u64, size: usize) -> Vec<u8> {
+    let mut buf = Vec::with_capacity(size);
+    buf.put_u8(b'v');
+    buf.put_u8(group);
+    buf.put_u64(i);
+    buf.put_slice(&vec![b' '; size - 10]);
+    buf
+}
+
+fn start_key(group: u8) -> Vec<u8> {
+    let mut buf = Vec::with_capacity(2);
+    buf.put_u8(b'k');
+    buf.put_u8(group);
+    buf
+}
+
+fn end_key(group: u8) -> Vec<u8> {
+    let mut buf = Vec::with_capacity(3);
+    buf.put_u8(b'k');
+    buf.put_u8(group);
+    buf.put_u8(255);
+    buf
+}
+
+struct ClusterInitializer {
+    client: ControlServiceClient<Channel>,
+    wheels: HashMap<u64, Endpoint>,
+    key_ranges: Vec<KeyRangeInfo>,
+}
+
+impl ClusterInitializer {
+    fn new(client: ControlServiceClient<Channel>) -> Self {
+        Self {
+            client,
+            wheels: HashMap::default(),
+            key_ranges: vec![],
+        }
+    }
+
+    fn add_wheel(&mut self, node: u64, host: String, port: u16) {
+        self.wheels.insert(
+            node,
+            Endpoint {
+                host,
+                port: port as u32,
+            },
+        );
+    }
+
+    fn add_key_range(
+        &mut self,
+        group: u64,
+        start_key: Vec<u8>,
+        end_key: Vec<u8>,
+        raft_nodes: HashMap<u64, u64>,
+    ) {
+        self.key_ranges.push(KeyRangeInfo {
+            group,
+            key_range: Some(KeyRange { start_key, end_key }),
+            raft_nodes,
+            leader: 0,
+        });
+    }
+
+    async fn init(mut self) {
+        let req = AddWheelsRequest {
+            wheels: self.wheels,
+        };
+        self.client.add_wheels(Request::new(req)).await.unwrap();
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        let req = AddKeyRangesRequest {
+            key_ranges: self.key_ranges,
+        };
+        self.client.add_key_ranges(Request::new(req)).await.unwrap();
+        tokio::time::sleep(Duration::from_secs(10)).await;
+    }
+}
+
+async fn assert_put(client: &RunkvClient, key: Vec<u8>, value: Vec<u8>) {
+    client.put(key, value).await.unwrap();
+}
+
+async fn assert_get(client: &RunkvClient, key: Vec<u8>, expected: Option<Vec<u8>>) {
+    let result = client.get(key).await.unwrap();
+    assert_eq!(result, expected);
+}
+
+async fn assert_delete(client: &RunkvClient, key: Vec<u8>) {
+    client.delete(key).await.unwrap();
+}
+
+async fn mkdir_if_not_exists(path: &str) {
+    if tokio::fs::metadata(path).await.is_ok() {
+        panic!("path {} already exists", path);
+    };
+    tokio::fs::create_dir_all(path).await.unwrap();
+}
+
+pub async fn run(args: Args, options: Options) {
+    // Prepare directories.
+    println!("Prepare directories...");
+    let ts = timestamp();
+    let log_dir = format!("{}-{}", args.log_dir, ts);
+    let raft_log_store_data_dir = format!("{}-{}", args.raft_log_store_data_dir, ts);
+    mkdir_if_not_exists(&log_dir).await;
+    mkdir_if_not_exists(&raft_log_store_data_dir).await;
+
+    // Init log.
+    println!("Init log...");
+    let log_guard = if options.log {
+        Some(init_runkv_logger("tests", 0, &log_dir))
+    } else {
+        None
+    };
+
+    // Read config templates.
+    println!("Read config templates...");
+    let rudder_config: RudderConfig = {
+        let mut config: RudderConfig = toml::from_str(&concat_toml(
+            &options.rudder_config_path,
+            &options.lsm_tree_config_path,
+        ))
+        .unwrap();
+        config.id = options.rudder_node_id;
+        config.host = LOCALHOST.to_string();
+        config.port = options.rudder_port;
+        config
+    };
+    let wheel_config_template: WheelConfig = {
+        let mut config: WheelConfig = toml::from_str(&concat_toml(
+            &options.wheel_config_path,
+            &options.lsm_tree_config_path,
+        ))
+        .unwrap();
+        config.rudder.id = options.rudder_node_id;
+        config.rudder.host = rudder_config.host.clone();
+        config.rudder.port = rudder_config.port;
+        config.raft_log_store.persist = args.persist;
+        config.host = LOCALHOST.to_string();
+        config
+    };
+    let exhauster_config_template: ExhausterConfig = {
+        let mut config: ExhausterConfig =
+            toml::from_str(&read_to_string(options.exhauster_config_path).unwrap()).unwrap();
+        config.rudder.id = options.rudder_node_id;
+        config.rudder.host = rudder_config.host.clone();
+        config.rudder.port = rudder_config.port;
+        config.host = LOCALHOST.to_string();
+        config
+    };
+
+    // Connect object store.
+    // TODO: Support S3.
+    println!("Connect object store...");
+    let object_store = Arc::new(MemObjectStore::default());
+
+    // Build and bootstrap rudder.
+    println!("Bootstrap rudder...");
+    let (rudder, rudder_workers) =
+        build_rudder_with_object_store(&rudder_config, object_store.clone())
+            .await
+            .unwrap();
+    let rudder_config_clone = rudder_config.clone();
+    tokio::spawn(
+        async move { bootstrap_rudder(&rudder_config_clone, rudder, rudder_workers).await },
+    );
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    // Build and bootstrap wheels.
+    for i in 1..=args.wheels {
+        println!("Bootstrap wheel {}...", i);
+        let wheel_config = {
+            let mut config = wheel_config_template.clone();
+            config.raft_log_store.log_dir_path = format!("{}/{}", raft_log_store_data_dir, i);
+            config.id = i + options.wheel_node_id_base;
+            config.port = i as u16 + options.wheel_port_base;
+            config.prometheus.port = i as u16 + options.wheel_prometheus_port_base;
+            config
+        };
+        let (wheel, wheel_workers) =
+            build_wheel_with_object_store(&wheel_config, object_store.clone())
+                .await
+                .unwrap();
+        tokio::spawn(async move { bootstrap_wheel(&wheel_config, wheel, wheel_workers).await });
+    }
+
+    // Build and bootstrap exhausters.
+    for i in 1..=args.exhausters {
+        println!("Bootstrap exhauster {}...", i);
+        let exhauster_config = {
+            let mut config = exhauster_config_template.clone();
+            config.id = i + options.exhauster_node_id_base;
+            config.port = i as u16 + options.exhauster_port_base;
+            config
+        };
+        let (exhuaster, exhauster_workers) =
+            build_exhauster_with_object_store(&exhauster_config, object_store.clone())
+                .await
+                .unwrap();
+        tokio::spawn(async move {
+            bootstrap_exhauster(&exhauster_config, exhuaster, exhauster_workers).await
+        });
+    }
+
+    // Initialize cluster.
+    println!("Init cluster...");
+    let client =
+        ControlServiceClient::connect(format!("http://{}:{}", LOCALHOST, rudder_config.port))
+            .await
+            .unwrap();
+    let mut initializer = ClusterInitializer::new(client);
+    for i in 1..=args.wheels {
+        initializer.add_wheel(
+            i + options.wheel_node_id_base,
+            LOCALHOST.to_string(),
+            i as u16 + options.wheel_port_base,
+        );
+    }
+    for (i, group) in (1..=args.groups).enumerate() {
+        let raft_nodes = HashMap::from_iter([
+            (
+                (i as u64) * 3 + 1,
+                (((i as u64) * 3) % args.wheels as u64) + 1 + options.wheel_node_id_base,
+            ),
+            (
+                (i as u64) * 3 + 2,
+                (((i as u64) * 3 + 1) % args.wheels as u64) + 1 + options.wheel_node_id_base,
+            ),
+            (
+                (i as u64) * 3 + 3,
+                (((i as u64) * 3 + 2) % args.wheels as u64) + 1 + options.wheel_node_id_base,
+            ),
+        ]);
+        let start_key = start_key(group);
+        let end_key = end_key(group);
+        println!(
+            "Add key range: [group: {}] [start key: {:?}] [end key: {:?}] [raft nodes: {:?}]",
+            group, start_key, end_key, raft_nodes,
+        );
+        initializer.add_key_range(group as u64, start_key, end_key, raft_nodes);
+    }
+    initializer.init().await;
+
+    let runkv_client = RunkvClient::open(RunkvClientOptions {
+        rudder: rudder_config.id,
+        rudder_host: rudder_config.host.clone(),
+        rudder_port: rudder_config.port,
+        heartbeat_interval: Duration::from_nanos(0),
+    })
+    .await;
+    runkv_client.update_router().await.unwrap();
+
+    let futures = (1..=args.groups)
+        .flat_map(|group| {
+            let runkv_client_clone = runkv_client.clone();
+            (1..=args.concurrency).map(move |c| {
+                let client_clone = runkv_client_clone.clone();
+                async move {
+                    let mut rng = thread_rng();
+
+                    let client = client_clone.clone();
+
+                    let key = key(group, c, args.key_size);
+                    let value = value(group, c, args.value_size);
+
+                    for _ in 0..args.r#loop {
+                        tokio::time::sleep(Duration::from_millis(rng.gen_range(0..10))).await;
+                        assert_put(&client, key.clone(), value.clone()).await;
+                        tokio::time::sleep(Duration::from_millis(rng.gen_range(0..10))).await;
+                        assert_get(&client, key.clone(), Some(value.clone())).await;
+                        tokio::time::sleep(Duration::from_millis(rng.gen_range(0..10))).await;
+                        assert_delete(&client, key.clone()).await;
+                        tokio::time::sleep(Duration::from_millis(rng.gen_range(0..10))).await;
+                        assert_get(&client, key.clone(), None).await;
+                        tokio::time::sleep(Duration::from_millis(rng.gen_range(0..10))).await;
+                        assert_put(&client, key.clone(), value.clone()).await;
+                        tokio::time::sleep(Duration::from_millis(rng.gen_range(0..10))).await;
+                        assert_get(&client, key.clone(), Some(value.clone())).await;
+                    }
+                }
+            })
+        })
+        .collect_vec();
+
+    let start = Instant::now();
+
+    println!("Start kv operations...");
+
+    future::join_all(futures).await;
+
+    println!("elapsed: {:.3?}", start.elapsed());
+    println!("Finish.");
+
+    drop(log_guard);
+}
