b27f17c8ecb0cb351f35edd7c7dfccf0e081fbf7
MrCroxx <mrcroxx@outlook.com>
2022-11-07 04:17:17 +0000

    feat: introduce tiered cache module

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -3,13 +3,18 @@
 #![feature(generators, generator_trait)]
 #![feature(stmt_expr_attributes)]
 #![feature(proc_macro_hygiene)]
+#![feature(trait_alias)]
+#![feature(let_chains)]
+#![feature(allocator_api)]
+#![feature(lint_reasons)]
+#![feature(build_hasher_simple_hash_one)]
+#![feature(strict_provenance)]
 
 mod error;
-#[cfg(target_os = "linux")]
-pub mod file_cache;
 mod lsm_tree;
 mod object_store;
 pub mod raft_log_store;
+pub mod tiered_cache;
 pub mod utils;
 
 pub use error::*;
--- storage/src/utils/mod.rs
+++ storage/src/utils/mod.rs
@@ -2,3 +2,4 @@ mod coding;
 pub use coding::*;
 mod bloom;
 pub use bloom::*;
+pub mod lru_cache;
